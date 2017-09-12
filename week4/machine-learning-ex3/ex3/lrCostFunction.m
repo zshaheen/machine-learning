e5b8a81a977b5a950053f4a73736617e1342ff91
Zeshawn <zshaheen93@gmail.com>
2017-09-11 23:06:19 -0700

    Done with regularized logistic regression.

--- week4/machine-learning-ex3/ex3/lrCostFunction.m
+++ week4/machine-learning-ex3/ex3/lrCostFunction.m
@@ -36,12 +36,29 @@ grad = zeros(size(theta));
 %           grad = grad + YOUR_CODE_HERE (using the temp variable)
 %
 
+% size(theta) == 3x1
+% size(X) == 100x3
+% size(y) == 100x1
+
+% Cost function J, where size(J) == 1x1
+% must have size(h) == size(y) == 100x1, b/c we do h-y later
+h = sigmoid(X*theta);  % (100x3)*(3x1) == 100x1
+
+diff1 = -y .* log(h);  % (100x1).*(100x1) = 100x1
+diff2 = (1-y) .* log(1-h);  % (100x1).*(100x1) = 100x1
+
+new_theta = theta(2:end);  % Don't include theta_0
+cost_sigma = (lambda/(2*m)) .* sum(new_theta.^2);
+
+J = (1/m) * sum(diff1 - diff2) + cost_sigma;  % size( sum((100x1)) ) == 1x1
 
 
+% Partial derivative grad, where size(grad) == 3x1
+grad_sigma = (lambda/m) * new_theta;
+grad_sigma = [0; grad_sigma];
+sum_term = h - y;  % size(sum_term) == 100x1
 
-
-
-
+grad = (1/m) * (X'*sum_term) + grad_sigma;  % (2x1) .* (3x100)*(100x1) = 3x1
 
 
 
